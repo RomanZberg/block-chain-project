781030eab84cbbabc62c43db7b48a8ca7453071b
Roman Zberg <roman@zberg.ch>
2023-12-15 15:40:30 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:21
+
+RUN echo "cd /app/frontend && npm i" >> /run.sh
+RUN echo "cd /app/ && npm i" >> /run.sh
+RUN echo "cd /app/frontend && npm start >~/frontend.log 2>&1 &" >> /run.sh
+RUN echo "cd /app && npx hardhat node  >~/hardhat.log 2>&1" >> /run.sh
+RUN chmod 770 /run.sh
+
+
+CMD ["/run.sh"]
---
+++ src/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.19",
+    networks: {
+        hardhat: {
+            accounts:
+                [
+                    {
+                        privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+                        balance: '1000000000000000000'
+                    },
+                    {
+                        privateKey: '0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0',
+                        balance: '5000000000000000000'
+                    }
+                ]
+        }
+    }
+};
