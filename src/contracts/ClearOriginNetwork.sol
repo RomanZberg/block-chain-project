ae447930c803eaff1e8c064b9e705a08bebac77e
luca <gafner99@gmail.com>
2023-12-21 14:21:17 +0100

    Push Changes

--- src/contracts/ClearOriginNetwork.sol
+++ src/contracts/ClearOriginNetwork.sol
@@ -4,79 +4,38 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "lib/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter public _tokenIds;
     bytes32 public constant COMPANY_ROLE = keccak256("COMPANY_ROLE");
     uint256 private _nextTokenId;
+
     struct Company {
         bytes32 companyName;
         address companyAddress;
         bytes32[] products;
     }
 
+    Company[] public companies;
+
+
+    // Allowed Products: {1x43483: ["Uhr", ""]
+
     constructor(address defaultAdmin) ERC721("ClearOriginNetwork", "CON") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
     }
 
-    mapping(address => Company) private companies;
-    address[] private companyAddressList;
+    function safeMint(address to, string memory uri) public onlyRole(COMPANY_ROLE) returns (uint256){
+        _tokenIds.increment();
+        uint256 id = _tokenIds.current();
 
-    function createCompany(bytes32 _companyName, address _companyAddress) public {
-        companies[_companyAddress] = Company(_companyName, _companyAddress, new bytes32[](0));
-        companyAddressList.push(_companyAddress);
-    }
+        _safeMint(to, id);
+        _setTokenURI(id, uri);
 
-    function addProducts(address _companyAddress, bytes32[] memory _products) public {
-        Company storage company = companies[_companyAddress];
-        for (uint256 i = 0; i < _products.length; i++) {
-            company.products.push(_products[i]);
-        }
-    }
-
-    function addProduct(address _companyAddress, bytes32 _product) public {
-        Company storage company = companies[_companyAddress];
-        company.products.push(_product);
-    }
-
-    function getCompanyProducts(address _companyAddress) public view returns (bytes32[] memory) {
-        return companies[_companyAddress].products;
-    }
-
-    function getCompaniesWithProduct(string memory _product) public view returns (address[] memory) {
-        uint256 count = 0;
-        for (uint256 i = 0; i < companyAddressList.length; i++) {
-            if (hasProduct(companyAddressList[i], _product)) {
-                count++;
-            }
-        }
-
-        address[] memory result = new address[](count);
-        uint256 index = 0;
-        for (uint256 i = 0; i < companyAddressList.length; i++) {
-            if (hasProduct(companyAddressList[i], _product)) {
-                result[index] = companyAddressList[i];
-                index++;
-            }
-        }
-
-        return result;
-    }
-
-    function hasProduct(address _companyAddress, string memory _product) public view returns (bool) {
-        Company storage company = companies[_companyAddress];
-        for (uint256 i = 0; i < company.products.length; i++) {
-            if (keccak256(abi.encodePacked(company.products[i])) == keccak256(abi.encodePacked(_product))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function safeMint(address to, string memory uri) public onlyRole(COMPANY_ROLE) {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        return id;
     }
 
     // Function to transfer NFT from one address to another --> Make a delivery with products
@@ -86,8 +45,33 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         safeTransferFrom(from, to, tokenId);
     }
 
+    // Function to get the array of addresse
+
+    function createCompany(bytes32 _companyName, address _companyAddress, bytes32[] memory _companyProducts) public {
+        companies.push(
+            Company(
+              _companyName,
+              _companyAddress,
+              _companyProducts
+            )
+        );
+    }
+
+    function getCompany(uint _index) public view returns(bytes32 companyName, address companyAddress,bytes32[] memory products ){
+        Company storage company = companies[_index];
+        return (company.companyName, company.companyAddress,company.products);
+    }
+    function getCompanies() public view returns(Company[] memory comp)
+    {
+        return companies;
+    }
+
+
+
+
 
     // The following functions are overrides required by Solidity.
+
     function tokenURI(uint256 tokenId)public view
     override(ERC721, ERC721URIStorage)
     returns (string memory)
