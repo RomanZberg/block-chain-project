6a29756df9729a611cd3411c5d991ad61fc869cd
luca <gafner99@gmail.com>
2023-12-22 18:20:25 +0100

    Added Comments and README.md

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
+# ClearOrigin 
+
+## Beschreibung
+### Smart Contract
+Unser Smart Contract ist unter [src/contracts/](src/contracts/ClearOriginNetwork.sol) zu finden.
+### Frontend
+Unser Frontend ist unter [src/app/](src/app/) in den verschiedenen Ordner abgespeichert.
+
+
+## Quellen
 https://ackarim.medium.com/get-your-first-ethers-in-angular-f8d9a62735ed
-
-
-
-
 
 
 
--- src/contracts/ClearOriginNetwork.sol
+++ src/contracts/ClearOriginNetwork.sol
@@ -11,13 +11,14 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
     bytes32 public constant COMPANY_ROLE = keccak256("COMPANY_ROLE");
     uint256 private _nextTokenId;
 
+    // Company Objekt. In diesem Objekt werden die Daten der Unternehmen gespeichert, welche durch uns auf der Blockchain verifiziert werden.
     struct Company {
         bytes32 companyName;
         address companyAddress;
         bytes32[] products;
         bool isValue;
     }
-
+    // DeliveryItem Objekt. In diesem Objekt werden die Produkte gespeichert inklusive der Anzahl.
     struct DeliveryItem {
         bytes32 name;
         uint8 numberOfItems;
@@ -25,7 +26,7 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
 
     address[] private admins;
 
-
+    // Konstruktur der Contract Klasse. Wir als Deployer geben uns die Admin Rolle.
     constructor(address defaultAdmin) ERC721("ClearOriginNetwork", "CON") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         admins.push(defaultAdmin);
@@ -33,17 +34,17 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
 
     mapping(address => Company) private companies;
 
-
     mapping(uint256 => DeliveryItem[]) private tokenIdDeliveryItemsMapping;
-
 
     address[] private  companyAddresses;
 
-
+    // Funktion um sämtliche Admins zu bekommen.
     function getAdmins() public view returns (address[] memory){
         return admins;
     }
 
+    // Nachdem ein Unternehmen von uns verifiziert wurde, bekommt es zu die Company Rolle um zu minten
+    // Die Funktin darf nur von einem Admin aufgerufen werden.
     function addCompany(bytes32 _companyName, address _companyAddress, bytes32[] memory _companyProducts)
     public onlyRole(DEFAULT_ADMIN_ROLE)
     {
@@ -61,6 +62,7 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         companyAddresses.push(_companyAddress);
     }
 
+    // Funktion um die Daten eines Unternehmens zu bekommen.
     function getCompany(address companyAddress) public view returns (bytes32, bytes32[] memory){
         require(companies[companyAddress].isValue == true, 'No Company with the requested wallet address');
 
@@ -68,7 +70,7 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         return (company.companyName, company.products);
     }
 
-
+    // Funktion um die Daten aller Unternehmen zu bekommen.
     function getCompanies() public view returns (bytes32[] memory, address[] memory, bytes32[][] memory)
     {
 
@@ -86,7 +88,7 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         return (companyNamesR, companyAddressesR, productsArrayR);
     }
 
-
+    // Funktion um die Daten einer Lieferung zu bekommen.
     function getDelivery(uint256 tokenId) public view returns (bytes32[] memory, uint8[] memory)
     {
 
@@ -104,7 +106,7 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         return (deliveryProductsR, numberOfItemsR);
     }
 
-
+    // Funktion um die Daten aller Lieferungen zu bekommen.
     function addProducts(address _companyAddress, bytes32[] memory _products) public {
         Company storage company = companies[_companyAddress];
         for (uint256 i = 0; i < _products.length; i++) {
@@ -112,12 +114,13 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         }
     }
 
-
+    // Funktion um ein Produkt zu einem Unternehmen hinzuzufügen.
     function addProduct(address _companyAddress, bytes32 _product) public {
         Company storage company = companies[_companyAddress];
         company.products.push(_product);
     }
 
+    // Diese Funktion gibt alle Produkte eines Unternehmens zurück, welches es minten darf
     function getCompanyProducts(address _companyAddress) public view returns (bytes32[] memory) {
         return companies[_companyAddress].products;
     }
@@ -152,6 +155,8 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         return false;
     }
 
+    // Funktion um ein NFT zu minten. Die Funktion darf nur von einem Unternehmen aufgerufen werden, nachdem es von uns überprüft hinzugefügt wurde.
+    // Um die Funktion aufzurufen, braucht es die Company Rolle.
     function safeMint(bytes32[] memory _deliveryProducts, uint8[] memory numberOfItems) public onlyRole(COMPANY_ROLE) {
         uint256 tokenId = _nextTokenId++;
 
@@ -162,15 +167,15 @@ contract ClearOriginNetwork is ERC721, ERC721URIStorage, ERC721Burnable, AccessC
         }
     }
 
-// Function to transfer NFT from one address to another --> Make a delivery with products
+    // Diese Funktion transferiert ein NFT von einem Unternehmen zu einem anderen. Also eine Lieferung.
+    // Es erfordert, dass es das NFT auch in Besitz hat.
     function transferNFT(address from, address to, uint256 tokenId) public {
         require(ownerOf(tokenId) == from, "Token not owned by the address");
 
         safeTransferFrom(from, to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
-
+    // Die folgenden Funktionen sind Überschreibungen, die von Solidity benötigt werden.
     function _update(address to, uint256 tokenId, address auth)
     internal
     override(ERC721, ERC721Enumerable)
